2b3fc1965c0cfbfd212626e9d56aa5c8e3129952
szk2s <suzukisjss@gmail.com>
2019-12-15 22:46:00 +0900

    Add MidiOut dawlet

--- satoshi/src/renderer/App.jsx
+++ satoshi/src/renderer/App.jsx
@@ -1,4 +1,11 @@
 // @flow
 import React from 'react';
+import './App.css';
+import { MidiOut } from './dawlets/MidiOut';
 
-export const App = () => <h1>Hello from React!</h1>;
+export const App = () => (
+  <div id="app">
+    <h1>Hello from React!</h1>
+    <MidiOut />
+  </div>
+);
---
+++ satoshi/src/renderer/dawlets/MidiOut.jsx
@@ -0,0 +1,4 @@
+// @flow
+import React from 'react';
+
+export const MidiOut = () => <h1>This is MidiOut</h1>;
