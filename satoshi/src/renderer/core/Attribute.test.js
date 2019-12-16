fbb031a664bbc0fae8a46d08e9a48fbdf54cd667
szk2s <suzukisjss@gmail.com>
2019-12-16 19:57:55 +0900

    add test for Attribute

---
+++ satoshi/src/renderer/core/Attribute.test.js
@@ -0,0 +1,12 @@
+import { Constant, Expression } from './Attribute';
+
+test('instanciates constant', () => {
+  const attribute = new Constant(3);
+  expect(attribute).toBeInstanceOf(Constant);
+  expect(attribute.value).toBe(3);
+});
+test('instanciates expression', () => {
+  const attribute = new Expression('1+1');
+  expect(attribute).toBeInstanceOf(Expression);
+  expect(attribute.value).toBe(2);
+});
