223309ec68bb05a0c471c9dd284e12218b73c6cb
szk2s <suzukisjss@gmail.com>
2019-12-18 14:22:29 +0900

    check mock functions

---
+++ satoshi/src/renderer/core/getFromContainer.test.js
@@ -0,0 +1,16 @@
+import getFromContainer from './Container';
+jest.mock('./Container');
+
+describe('a mock of getFromContainer', () => {
+  it('should return 42', () => {
+    getFromContainer.mockImplementation(() => 42);
+    expect(getFromContainer()).toBe(42);
+  });
+  it('should be a jest.fn', () => {
+    const mockFn = jest.fn(x => x * 2)
+    getFromContainer.mockImplementation(mockFn);
+    expect(getFromContainer(2)).toBe(4);
+    expect(mockFn).toBeCalledTimes(1);
+    expect(mockFn).toBeCalledWith(2);
+  });
+});
