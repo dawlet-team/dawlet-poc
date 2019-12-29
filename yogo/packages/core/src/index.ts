f944e18f946de229aa347365cab5bd256684e307
Yuichi Yogo <yuichkun@gmail.com>
2019-12-29 12:37:17 +0900

    feat(core): add hello-world

---
+++ yogo/packages/core/src/index.ts
@@ -0,0 +1,25 @@
+import { ApolloServer, gql } from 'apollo-server'
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
