376588acf0df07524fcad065d271581c98e0eb60
szk2s <suzukisjss@gmail.com>
2020-01-19 16:47:29 +0900

    implement redux store

--- satoshi/src/renderer/index.js
+++ satoshi/src/renderer/index.js
@@ -3,10 +3,18 @@ import { App } from './App';
 import React from 'react';
 import { containerManager } from './core/ContainerManager';
 import { insertMockData } from './score/mock';
+import { createStore } from './redux/createStore';
+import { Provider } from 'react-redux';
 
 containerManager.initContainer();
 insertMockData();
+const store = createStore();
 
 window.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
 });
