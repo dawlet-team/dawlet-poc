c1d6a2cc96d76e6242830fb034f9a46a00756e0e
Yuichi Yogo <yuichkun@gmail.com>
2020-01-19 21:36:24 +0900

    feat: add install-devtools extension

--- yogo/packages/common-utils/src/index.ts
+++ yogo/packages/common-utils/src/index.ts
@@ -1,5 +1,6 @@
 import *  as fs from 'fs'
 import { join } from 'path'
+import installExtension, { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } from 'electron-devtools-installer';
 
 export const fetchAvailableDawlets = () => {
   return filterUnrelatedFolders(readPackagesFolder())
@@ -10,6 +11,16 @@ export const readPackagesFolder = () => {
 }
 
 
-const UNRELATED_FOLDERS = ['common-utils',  'core', 'launcher', 'react-electron-webpack-config']
+const UNRELATED_FOLDERS = ['common-utils', 'core', 'launcher', 'react-electron-webpack-config']
 
 export const filterUnrelatedFolders = (folders: string[]) => folders.filter(folder => !UNRELATED_FOLDERS.includes(folder))
+
+
+export function installDevTools() {
+  installExtension(REACT_DEVELOPER_TOOLS)
+    .then(name => console.log(`Added Extension:  ${name}`))
+    .catch(err => console.log('An error occurred: ', err));
+  installExtension(REDUX_DEVTOOLS)
+    .then(name => console.log(`Added Extension:  ${name}`))
+    .catch(err => console.log('An error occurred: ', err));
+}
