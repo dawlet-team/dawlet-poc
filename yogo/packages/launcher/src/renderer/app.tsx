9baca9b0cb70401cd74d7f526f0df91ade7d8605
Yuichi Yogo <yuichkun@gmail.com>
2020-01-21 01:23:57 +0900

    feat: enable selecting dawlets

--- yogo/packages/launcher/src/renderer/app.tsx
+++ yogo/packages/launcher/src/renderer/app.tsx
@@ -1,22 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback } from "react";
+import { useConfiguredDawlets } from './hooks/dawlets'
+import { Button, Typography } from '@material-ui/core'
+import { DawletList } from './components/DawletList'
 import { fetchAvailableDawlets } from "common-utils";
 
-const App = () => {
-  const [dawlets, setDawlets] = useState<Array<string>>([]);
+// Very very bad.
+const availableDawlets = fetchAvailableDawlets()
 
-  useEffect(() => {
-    const availableDawlets = fetchAvailableDawlets();
-    setDawlets(availableDawlets);
-  }, []);
+const App = () => {
+  const { configuredDawlets, checkDawlet } = useConfiguredDawlets(availableDawlets)
+  console.log('configured dawlets', configuredDawlets)
+
+  const bootDawlets = useCallback(() => {
+    const enabledDawlets = Object.values(configuredDawlets).filter(dawlet => dawlet.checked)
+    console.log('enabled dawlets', enabledDawlets)
+    // TODOs:
+    // 1. filter for localized dawlets
+    // 2. spawn local dawlets
+    // 3. boot server
+    // 4. show server url
+  }, [configuredDawlets])
 
   return (
     <div>
-      <h1>Dawlet Launcher</h1>
-      <ul>
-        {dawlets.map(dawlet => (
-          <li>{dawlet}</li>
-        ))}
-      </ul>
+      <Typography variant="h1">Dawlet Launcher</Typography>
+      <DawletList configuredDawlets={configuredDawlets} checkDawlet={checkDawlet} />
+      <Button variant="contained" color="primary" onClick={bootDawlets}>Launch</Button>
     </div>
   );
 };
