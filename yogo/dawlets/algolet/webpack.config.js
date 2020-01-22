a409a83b04f033a8a3dba4d9b1a05917f654d2ff
Yuichi Yogo <yuichkun@gmail.com>
2020-01-23 01:16:28 +0900

    feat: setup react-monaco-editor

--- yogo/dawlets/algolet/webpack.config.js
+++ yogo/dawlets/algolet/webpack.config.js
@@ -1,3 +1,12 @@
-const config = require('react-electron-webpack-config')
+const [main, _renderer] = require('react-electron-webpack-config')
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
+const renderer = {
+  ..._renderer,
+  plugins: [
+    ..._renderer.plugins,
+    new MonacoWebpackPlugin() 
+  ]
+}
+const config = [main, renderer]
 module.exports = config
