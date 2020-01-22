d6c08a48b8e54270a53b81a86981d19fa8dc0a1d
Yuichi Yogo <yuichkun@gmail.com>
2020-01-23 02:00:15 +0900

    feat: add monaco-editor inside algolet

--- yogo/dawlets/algolet/src/renderer/app.tsx
+++ yogo/dawlets/algolet/src/renderer/app.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import { gql } from 'apollo-boost';
-import { Query } from 'react-apollo';
+import React from "react";
+import { gql } from "apollo-boost";
+import { Query } from "react-apollo";
+import MonacoEditor from "react-monaco-editor";
 
 const GET_HELLO = gql`
   query {
     hello
   }
-`
+`;
 
 const App = () => (
   <Query query={GET_HELLO}>
     {({ loading, error, data }: any) => {
       if (loading) return <div>Loading...</div>;
-      if (error) return <div>Error :(</div>;
+      if (error) console.error(error);
+      // const code = this.state.code;
+      const code = "hogehoge";
+      const options = {
+        selectOnLineNumbers: true
+      };
+
+      /**
+       * TODO:
+       * should send mutations on enter or something.
+       */
+      const onChange = (e: any) => {
+        console.log("onchange");
+        console.log(e);
+      };
+      const onDidMount = (e: any) => {
+        console.log("ondidmount");
+        console.log(e);
+      };
 
       return (
-        <div>{JSON.stringify(data)}</div>
-      )
+        <MonacoEditor
+          width="800"
+          height="600"
+          language="javascript"
+          theme="vs-dark"
+          value={code}
+          options={options}
+          onChange={onChange}
+          editorDidMount={onDidMount}
+        />
+      );
     }}
   </Query>
-)
+);
 
-export default App
+export default App;
