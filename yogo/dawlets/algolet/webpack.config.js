82daf0bf82f485eb264e00cec9e3885157350ad9
Yuichi Yogo <yuichkun@gmail.com>
2020-01-23 01:38:35 +0900

    fix: monaco webpack config

--- yogo/dawlets/algolet/webpack.config.js
+++ yogo/dawlets/algolet/webpack.config.js
@@ -1,12 +1,27 @@
 const [main, _renderer] = require('react-electron-webpack-config')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
+const monacoRules = [{
+  test: /\.css$/,
+  use: ['style-loader', 'css-loader']
+}, {
+  test: /\.ttf$/,
+  use: ['file-loader']
+}]
 const renderer = {
   ..._renderer,
   plugins: [
     ..._renderer.plugins,
-    new MonacoWebpackPlugin() 
-  ]
+    new MonacoWebpackPlugin({
+      languages: ['javascript']
+    }) 
+  ],
+  module: {
+    rules: [
+      ..._renderer.module.rules,
+      ...monacoRules
+    ]
+  }
 }
 const config = [main, renderer]
 module.exports = config
