7da852451c9ae70cb2c8620c4acfa7609004541b
Yuichi Yogo <yuichkun@gmail.com>
2020-01-26 03:03:25 +0900

    feat: add standalone sheetlet

---
+++ yogo/dawlets/sheetlet/src/renderer/app.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { gql } from "apollo-boost";
+import { Query } from "react-apollo";
+import { join } from "path";
+import { readFileSync } from "fs";
+import { SheetMusicViewer } from "sheetmusic-viewer";
+
+const GET_HELLO = gql`
+  query {
+    hello
+  }
+`;
+
+const sampleFile = readFileSync(
+  join(process.cwd(), "../../packages/sheetmusic-viewer/public/sample.xml"),
+  { encoding: "utf-8" }
+);
+
+const App = () => (
+  <Query query={GET_HELLO}>
+    {({ loading, error, data }: any) => {
+      if (loading) return <div>Loading...</div>;
+      if (error) console.error(error);
+      return (
+        <SheetMusicViewer options={{ autoResize: true }} file={sampleFile} />
+      );
+    }}
+  </Query>
+);
+
+export default App;
---
+++ yogo/dawlets/sheetlet/webpack.config.js
@@ -0,0 +1,1 @@
+module.exports = require('react-electron-webpack-config')
