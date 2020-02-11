852b96d8d2be97be287a3a593eee1122cb19b70a
Yuichi Yogo <yuichkun@gmail.com>
2020-02-12 01:06:52 +0900

    WIP: add definitions

--- yogo/packages/core/index.d.ts
+++ yogo/packages/core/index.d.ts
@@ -1,9 +1,53 @@
 declare namespace IScore {
-  export interface Entity {
-    id: string
+  interface Entity {
+    /**
+     * All the notes present in the score.
+     */
+    notes: INote.Entity[]
   }
 
-  export interface Service {
+  interface Service {
+    /**
+     * Retrieves the full score.
+     */
     get(): Entity
+    /**
+     * appends a note to score
+     */
+    append(note: INote.Entity): Entity
   }
 } 
+declare namespace INote {
+  interface Entity {
+    /**
+     * unique identifier
+     */
+    id: string
+    /**
+     * Pitch represented in Hz
+     */
+    freq: number
+    /**
+     * Duration of the note
+     */
+    duration: number
+    /**
+     * Offset of the note from the beginning of the score.
+     */
+    offset: number
+  }
+
+  type createNoteInputs = {
+    freq: number
+    duration: number
+    offset: number
+  }
+  
+  interface Service {
+    /**
+     * create a new note
+     */
+    create(input: createNoteInputs): Entity
+  }
+
+}
