6d08175864167eb4fd7f2a8ab63ebed5e8a200c1
Yuichi Yogo <yuichkun@gmail.com>
2020-02-16 05:26:46 +0900

    feat: add a simple synth

--- yogo/dawlets/synthlet/src/renderer/app.tsx
+++ yogo/dawlets/synthlet/src/renderer/app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import { useSubscription } from "react-apollo";
+import { Synth } from './synth'
 
 const UPDATE_SCORE_SUBSCRIPTION = gql`
   subscription {
@@ -20,7 +21,14 @@ const App = () => {
   if (loading) return <div>Loading...</div>;
   if (error) console.error(error);
   console.log(data);
-  return <h1>Synthlet</h1>;
+  return (
+    <div>
+      <h1>Synthlet</h1>
+      {JSON.stringify(data)}
+      <Synth />
+    </div>
+  )
+
 };
 
 export default App;
