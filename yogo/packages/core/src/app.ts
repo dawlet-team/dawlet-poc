2c6c889072fe353638efbd93252dd506b5c9102f
Yuichi Yogo <yuichkun@gmail.com>
2020-03-21 07:07:34 +0900

    refactor: isolate boostrap code

--- yogo/packages/core/src/app.ts
+++ yogo/packages/core/src/app.ts
@@ -1,19 +1,3 @@
-import "reflect-metadata";
-import { ApolloServer } from 'apollo-server'
-import { assembleSchema } from './modules/schema'
-import { prepareContainer } from './container'
-
-async function bootstrap() {
-  prepareContainer()
-  const schema = await assembleSchema() 
-  const server = new ApolloServer({
-    schema,
-    playground: true
-  });
-
-  // Start the server
-  const { url } = await server.listen(4000);
-  console.log(`Server is running, GraphQL Playground available at ${url}`);
-}
+import { bootstrap } from './bootstrap'
 
 bootstrap()
---
+++ yogo/packages/core/src/bootstrap.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import { ApolloServer } from 'apollo-server'
+import { assembleSchema } from './modules/schema'
+import { prepareContainer } from './container'
+
+export async function bootstrap() {
+  prepareContainer()
+  const schema = await assembleSchema() 
+  const server = new ApolloServer({
+    schema,
+    playground: true
+  });
+
+  // Start the server
+  const { url } = await server.listen(4000);
+  console.log(`Server is running, GraphQL Playground available at ${url}`);
+  return server
+}
