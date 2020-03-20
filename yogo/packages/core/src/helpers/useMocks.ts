6ff1d434a3c2fcdf9d3abe668d58a9127fee3f32
Yuichi Yogo <yuichkun@gmail.com>
2020-03-21 06:12:35 +0900

    feat: enable Group

--- yogo/packages/core/src/helpers/useMocks.ts
+++ yogo/packages/core/src/helpers/useMocks.ts
@@ -4,6 +4,9 @@ const initialScore: Dawlet.IScore.Entity = {
   notes: []
 }
 
+const groupMap:Dawlet.IGroup.Map = {}
+
 export const mockContainer = () => {
   Container.set({id: "SCORE", value: initialScore})
+  Container.set({id: "GROUP", value: groupMap})
 }
--- yogo/packages/core/src/modules/schema.ts
+++ yogo/packages/core/src/modules/schema.ts
@@ -3,11 +3,12 @@ import { buildSchema } from 'type-graphql'
 import { Container } from 'typedi'
 import { ScoreResolver } from './score/resolver'
 import { NoteResolver } from './note/resolver'
+import { GroupResolver } from './group/resolver'
 
 export function assembleSchema() {
   return buildSchema(
     {
-      resolvers: [ScoreResolver, NoteResolver],
+      resolvers: [ScoreResolver, NoteResolver, GroupResolver],
       emitSchemaFile: path.resolve(process.cwd(), "schema.gql"),
       container: Container
     }
