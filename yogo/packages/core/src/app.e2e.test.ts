eb695ca80c57fc6ffe77a07a471155fc4ec4fa9b
Yuichi Yogo <yuichkun@gmail.com>
2020-03-21 07:26:27 +0900

    chore: add createGroup test

--- yogo/packages/core/src/app.e2e.test.ts
+++ yogo/packages/core/src/app.e2e.test.ts
@@ -1,28 +1,58 @@
-import { bootstrap } from './bootstrap'
-import { ApolloServer } from 'apollo-server'
-import { createTestClient, ApolloServerTestClient } from 'apollo-server-testing'
+import { bootstrap } from "./bootstrap";
+import { ApolloServer } from "apollo-server";
+import {
+  createTestClient,
+  ApolloServerTestClient
+} from "apollo-server-testing";
 
-describe('e2e', () => {
-  let server: ApolloServer
-  let url: string
-  let client: ApolloServerTestClient | null = null
+describe("e2e", () => {
+  let server: ApolloServer;
+  let url: string;
+  let client: ApolloServerTestClient | null = null;
 
   beforeAll(async () => {
-    console.log('bootstrapping a server');
-    ({ server, url } = await bootstrap())
-    client = createTestClient(server)
-  })
+    console.log("bootstrapping a server");
+    ({ server, url } = await bootstrap());
+    client = createTestClient(server);
+  });
   afterAll(async () => {
-    console.log('shutting down the server')
-    await server.stop()
-    client = null
-  })
-  it('server is defined', () => {
-    expect(server).toBeDefined()
-    expect(url).toBe("http://localhost:4000/")
-  })
-  it('client is defiend', () => {
-    expect(client).toBeDefined()
-  })
-   
-})
+    console.log("shutting down the server");
+    await server.stop();
+    client = null;
+  });
+  it("server is defined", () => {
+    expect(server).toBeDefined();
+    expect(url).toBe("http://localhost:4000/");
+  });
+  it("client is defiend", () => {
+    expect(client).toBeDefined();
+  });
+
+  describe("Group", () => {
+    it("creates group", async () => {
+      const res = await client?.query({
+        query: `
+        mutation {
+          createGroup(id: "hoge"){
+            id
+            notes{
+              id
+              freq
+              duration
+              offset
+            }
+          }
+        }
+        `
+      });
+      expect(res?.data).toMatchInlineSnapshot(`
+        Object {
+          "createGroup": Object {
+            "id": "hoge",
+            "notes": Array [],
+          },
+        }
+      `);
+    });
+  });
+});
