9f91d1a726d4aec2b1745239a2c12445147e0599
Yuichi Yogo <yuichkun@gmail.com>
2020-03-29 06:37:04 +0900

    chore: apply prettier

--- yogo/packages/utils/src/fs.test.ts
+++ yogo/packages/utils/src/fs.test.ts
@@ -1,10 +1,10 @@
 import { fetchAvailableDawlets, readDawletsDir, getPackageJson, retrieveDawletConfig, pathToDawletsDir } from './fs'
 
-describe('path to dawlets folder', () => {
-  it('contains an absolute path', () => {
-    expect(pathToDawletsDir).toMatch(/\/dawlet-poc\/yogo\/dawlets$/)
-  })
-})
+describe("path to dawlets folder", () => {
+  it("contains an absolute path", () => {
+    expect(pathToDawletsDir).toMatch(/\/dawlet-poc\/yogo\/dawlets$/);
+  });
+});
 
 describe('fetchAvailableDawlets', () => {
   it('reads dawlets folder', () => {
@@ -16,16 +16,26 @@ describe('fetchAvailableDawlets', () => {
   })
 })
 
-describe('getPackageJson', () => {
-  it('gets package json', () => {
-    const json = getPackageJson("algolet")
-    expect(json).toBeInstanceOf(Object)
-  })
-})
+describe("fetchAvailableDawlets", () => {
+  it("reads dawlets folder", () => {
+    const dawlets = readDawletsDir();
+    expect(dawlets).toMatchSnapshot();
+  });
+  it("fetches available dawlets", () => {
+    expect(fetchAvailableDawlets()).toMatchSnapshot();
+  });
+});
 
-describe('retrieveDawletConfig', () => {
-  it('gets dawlet config', () => {
-    const algoletConfig = retrieveDawletConfig("algolet")
-    expect(algoletConfig).toMatchSnapshot()
-  })
-})
+describe("getPackageJson", () => {
+  it("gets package json", () => {
+    const json = getPackageJson("algolet");
+    expect(json).toBeInstanceOf(Object);
+  });
+});
+
+describe("retrieveDawletConfig", () => {
+  it("gets dawlet config", () => {
+    const algoletConfig = retrieveDawletConfig("algolet");
+    expect(algoletConfig).toMatchSnapshot();
+  });
+});
