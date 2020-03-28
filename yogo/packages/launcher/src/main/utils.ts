7903e2fefac8133858d491b9009f93d33649dd38
Yuichi Yogo <yuichkun@gmail.com>
2020-03-29 06:42:26 +0900

    fix: dawlet launcher

--- yogo/packages/launcher/src/main/utils.ts
+++ yogo/packages/launcher/src/main/utils.ts
@@ -1,9 +1,9 @@
 
 import { join } from 'path'
 import { spawn } from 'child_process'
-import { pathToDawletsDir } from '@dawlet/utils'
+import { pathToDawletsDir, pkgNameWithoutPrefix } from '@dawlet/utils'
 
 export const spawnDawlet = (dawletName: string) => {
-  const args = join(pathToDawletsDir, dawletName)
+  const args = join(pathToDawletsDir, pkgNameWithoutPrefix(dawletName))
   spawn("electron", [args])
 }
