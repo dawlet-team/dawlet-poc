4daf608db2915011ef510712b62822aefbab4189
Yuichi Yogo <yuichkun@gmail.com>
2020-03-29 03:15:00 +0900

    chore: rename common-utils to @dawlet/utils

--- yogo/packages/launcher/src/main/utils.ts
+++ yogo/packages/launcher/src/main/utils.ts
@@ -1,7 +1,7 @@
 
 import { join } from 'path'
 import { spawn } from 'child_process'
-import { pathToDawletsDir } from 'common-utils'
+import { pathToDawletsDir } from '@dawlet/utils'
 
 export const spawnDawlet = (dawletName: string) => {
   const args = join(pathToDawletsDir, dawletName)
