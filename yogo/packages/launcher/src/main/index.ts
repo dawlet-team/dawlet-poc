a68da97eff3b833426735929f858b91c12a70f95
Yuichi Yogo <yuichkun@gmail.com>
2020-03-31 01:22:49 +0900

    feat(launcher): bootstrap core server

--- yogo/packages/launcher/src/main/index.ts
+++ yogo/packages/launcher/src/main/index.ts
@@ -2,6 +2,7 @@ import { app, BrowserWindow, ipcMain } from 'electron'
 import { installDevTools } from '@dawlet/utils'
 import { spawnDawlet } from './utils'
 import { DawletIPC } from '../common/types'
+import { bootstrap } from '@dawlet/core'
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -20,11 +21,8 @@ const onSpawnDawlet: DawletIPC.spawnDawlet = (_, name) => spawnDawlet(name)
 
 const main = () => {
   app.on('ready', createWindow);
-  /**
-   * TODO:
-   * should start a core server, too.
-   */
   ipcMain.on(DawletIPC.events.SPAWN_DAWLET, onSpawnDawlet) 
+  bootstrap()
 }
  
 main()
