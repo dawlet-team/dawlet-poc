5b7f913b4b183196304b0adeebf8bf17249aff39
Yuichi Yogo <yuichkun@gmail.com>
2020-03-30 23:12:03 +0900

    chore(algolet): make window size bigger

--- yogo/dawlets/algolet/src/main/index.ts
+++ yogo/dawlets/algolet/src/main/index.ts
@@ -4,8 +4,8 @@ import { installDevTools } from '@dawlet/utils'
 function createWindow () {
   // Create the browser window.
   let win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       nodeIntegration: true
     }
