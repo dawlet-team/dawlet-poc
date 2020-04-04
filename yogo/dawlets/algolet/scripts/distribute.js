9322ffcd7399693e4f2c557676a63cdd7bc270e6
Yuichi Yogo <yuichkun@gmail.com>
2020-04-05 08:12:39 +0900

    WIP

--- yogo/dawlets/algolet/scripts/distribute.js
+++ yogo/dawlets/algolet/scripts/distribute.js
@@ -1,45 +1,47 @@
-const { execSync } = require("child_process")
+// const { execSync } = require("child_process")
 
-const run = (cmd, cwd) => execSync(cmd, { encoding: "utf8", stdio: "inherit", cwd });
-const setEnv = (name, value) => {
-	if (value) {
-		process.env[name.toUpperCase()] = value.toString();
-	}
-};
-const getEnv = name => process.env[name.toUpperCase()] || null;
-const getInput = (name, required) => {
-	const value = getEnv(name);
-	if (required && !value) {
-		throw new Error(`"${name}" input variable is not defined`);
-	}
-	return value;
-};
-const getPlatform = () => {
-	switch (process.platform) {
-		case "darwin":
-			return "mac";
-		case "win32":
-			return "windows";
-		default:
-			return "linux";
-	}
-};
+// const run = (cmd, cwd) => execSync(cmd, { encoding: "utf8", stdio: "inherit", cwd });
+// const setEnv = (name, value) => {
+// 	if (value) {
+// 		process.env[name.toUpperCase()] = value.toString();
+// 	}
+// };
+// const getEnv = name => process.env[name.toUpperCase()] || null;
+// const getInput = (name, required) => {
+// 	const value = getEnv(name);
+// 	if (required && !value) {
+// 		throw new Error(`"${name}" input variable is not defined`);
+// 	}
+// 	return value;
+// };
+// const getPlatform = () => {
+// 	switch (process.platform) {
+// 		case "darwin":
+// 			return "mac";
+// 		case "win32":
+// 			return "windows";
+// 		default:
+// 			return "linux";
+// 	}
+// };
 
-const platform = getPlatform()
-console.log(`creating distribution for ${platform}`)
+// const platform = getPlatform()
+// console.log(`creating distribution for ${platform}`)
 
-if (platform === 'mac') {
-  setEnv("CSC_LINK", getInput("mac_csc_link", true));
-  setEnv("CSC_KEY_PASSWORD", getInput("mac_csc_link_password", true));
-  run(
-		`npx electron-builder --${platform}`,
-		// `CSC_LINK=${process.env.MAC_CSC_LINK} CSC_KEY_PASSWORD=${process.env.MAC_CSC_LINK_PASSWORD} npx electron-builder --${platform} --publish always`,
-		process.cwd(),
-	);
-}
-if (platform === 'windows') {
-  throw new Error('not supported yet') // TODO
-}
-if (platform === 'linux') {
-  throw new Error('not supported yet') // TODO
-}
+// if (platform === 'mac') {
+//   setEnv("CSC_LINK", getInput("mac_csc_link", true));
+//   setEnv("CSC_KEY_PASSWORD", getInput("mac_csc_link_password", true));
+//   run(
+// 		`npx electron-builder --${platform}`,
+// 		// `CSC_LINK=${process.env.MAC_CSC_LINK} CSC_KEY_PASSWORD=${process.env.MAC_CSC_LINK_PASSWORD} npx electron-builder --${platform} --publish always`,
+// 		process.cwd(),
+// 	);
+// }
+// if (platform === 'windows') {
+//   throw new Error('not supported yet') // TODO
+// }
+// if (platform === 'linux') {
+//   throw new Error('not supported yet') // TODO
+// }
+
+console.log(process.env["mac_csc_link"])
