974f12a295cd12ead0e15bf75954ce1776642583
Yuichi Yogo <yuichkun@gmail.com>
2020-04-17 05:11:02 +0900

    test(core): add setup file

--- yogo/packages/core/jest.config.js
+++ yogo/packages/core/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ["/node_modules/", "lib", "__tests__/helpers"]
+  testPathIgnorePatterns: ["/node_modules/", "lib", "__tests__/helpers"],
+  setupFiles: ['./jest.setup.ts']
 };
