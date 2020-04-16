055042feeb3f6ebf04b848accddd241fddf95108
Yuichi Yogo <yuichkun@gmail.com>
2020-04-17 06:45:43 +0900

    build(core): collect coverage

--- yogo/packages/core/jest.config.js
+++ yogo/packages/core/jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ["/node_modules/", "lib", "__tests__/helpers"],
-  setupFiles: ['./jest.setup.ts']
+  setupFiles: ['./jest.setup.ts'],
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**/*.ts']
 };
