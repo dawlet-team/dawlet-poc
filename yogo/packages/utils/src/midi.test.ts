d010cad2a81e2ab600882eca43cd3c34bd1ef145
szk2s <suzukisjss@gmail.com>
2020-05-08 21:10:31 +0900

    implement noteToMidi

--- yogo/packages/utils/src/midi.test.ts
+++ yogo/packages/utils/src/midi.test.ts
@@ -1,12 +1,19 @@
 import {
   noteToMidi
 } from "./midi";
-import { NoteFactory } from "../../engine/src/modules/note/factory";
+import { NoteFactory } from "../../factory/lib";
 
 describe("noteToMidi", () => {
   it("converts a single note into some MIDI messages and offset times", () => {
-    const result = noteToMidi(NoteFactory.build());
-    expect(result).toEqual([{ midiMessage: [60, 60, 60], offset: 1000}])
+    const result = noteToMidi({
+      id: 'test-note',
+      freq: 440,
+      duration: 1000,
+      offset: 500
+    });
+    expect(result[0].midiMessage).toEqual([144, 69, 100]);
+    expect(result[0].offset).toEqual(500);
+    expect(result[1].midiMessage).toEqual([128, 69, 100]);
+    expect(result[1].offset).toEqual(1500);
   });
 });
-
--- yogo/packages/utils/src/midi.ts
+++ yogo/packages/utils/src/midi.ts
@@ -1,3 +1,5 @@
+import * as JZZ from 'jzz'
+
 type int = number;
 type uint = number;
 type float = number;
@@ -13,6 +15,7 @@ type NormalizedFloat = number;
 type Id = uint;
 type Sec = ufloat;
 type Ms = ufloat;
+/** the unit is Hz */
 type Freq = ufloat;
 
 declare namespace Midi {
@@ -23,10 +26,44 @@ declare namespace Midi {
   type Message = [StatusByte, DataByte] | [StatusByte, DataByte, DataByte]
 }
 
+const DEFAULT_VELOCITY = 100;
+const DEFAULT_CHANNEL = 1;
+
+/** Convert frequency into MIDI NoteNumber */
+const ftom = (freq: Freq): Midi.NoteNumber => {
+  const getBaseLog = (x, y) => {
+    return Math.log(y) / Math.log(x);
+  };
+  const midiNoteNumber = 12 * getBaseLog(2, freq / 440) + 69;
+  if (midiNoteNumber < 0) {
+    return 0;
+  }
+  if (midiNoteNumber > 127) {
+    return 127;
+  }
+  return midiNoteNumber;
+};
+
+type FIX_ME = any;
+
+const jzzMidiToMidiMessage = (jzzMidi: FIX_ME): Midi.Message => jzzMidi.map(v => v);
+
 export const groupsToMidi = (groups: Dawlet.IGroup.Entity[]): { midiMessage: Midi.Message, offset: Ms}[] => {
   return [{ midiMessage: [60, 60, 60], offset: 1000}]
 };
 
 export const noteToMidi = (note: Dawlet.INote.Entity): { midiMessage: Midi.Message, offset: Ms}[] => {
-  return [{ midiMessage: [60, 60, 60], offset: 1000}]
+  const noteNumber = ftom(note.freq);
+  const noteOnMsg = jzzMidiToMidiMessage(JZZ.MIDI.noteOn(DEFAULT_CHANNEL - 1, noteNumber, DEFAULT_VELOCITY));
+  const noteOffMsg = jzzMidiToMidiMessage(JZZ.MIDI.noteOff(DEFAULT_CHANNEL - 1, noteNumber, DEFAULT_VELOCITY));
+  return [
+    {
+      midiMessage: noteOnMsg as FIX_ME,
+      offset: note.offset
+    },
+    {
+      midiMessage: noteOffMsg as FIX_ME,
+      offset: note.offset + note.duration
+    }
+  ];
 };
