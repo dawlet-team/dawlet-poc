fd2075ab86501ce74f3991e638e386afe7e89776
Yuichi Yogo <yuichkun@gmail.com>
2020-09-07 11:40:32 +0900

    fix(algolet): set enableRemoteModule to true

--- yogo/dawlets/algolet/main/index.ts
+++ yogo/dawlets/algolet/main/index.ts
@@ -14,7 +14,8 @@ function createWindow () {
     width: 1200,
     height: 800,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      enableRemoteModule: true
     },
     title
   });
