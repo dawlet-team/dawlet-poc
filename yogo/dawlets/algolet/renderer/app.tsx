4c3be0ca1430164cbc4a86956aa491d861b378d5
Yuichi Yogo <yuichkun@gmail.com>
2020-09-13 05:36:41 +0900

    improvement(algolet): improve editor resizing

--- yogo/dawlets/algolet/renderer/app.tsx
+++ yogo/dawlets/algolet/renderer/app.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { remote } from "electron";
+import { SheetMusicViewer } from "@dawlet/ui";
 import { gql } from "apollo-boost";
+import { remote } from "electron";
+import * as monacoEditor from "monaco-editor/esm/vs/editor/editor.api";
+import React, { useState } from "react";
 import { useQuery } from "react-apollo";
 import MonacoEditor from "react-monaco-editor";
 import SplitPane from "react-split-pane";
 // @ts-ignore
 import Pane from "react-split-pane/lib/Pane";
-import { SheetMusicViewer } from "@dawlet/ui";
+import { bindCommands } from "./editor/commands";
 // @ts-ignore
 import sampleFile from "./sample.xml"; // FIX_ME: hackie escape
-import * as monacoEditor from "monaco-editor/esm/vs/editor/editor.api";
-import { bindCommands } from "./editor/commands";
 
 const LIST_ALL_GROUPS = gql`
   query {
@@ -28,6 +28,7 @@ const LIST_ALL_GROUPS = gql`
 
 const App = () => {
   const { data, loading, error } = useQuery(LIST_ALL_GROUPS);
+  const [editor, setEditor] = useState<monacoEditor.editor.IStandaloneCodeEditor | null>(null)
 
   if (loading) return <div>Loading...</div>;
   if (error) console.error(error);
@@ -52,33 +53,48 @@ const App = () => {
   const allGroups = await sdk.ListAllGroups()
   console.log('all groups', allGroups)
       `;
-  console.log({data})
 
-  const onDidMount = (editor: monacoEditor.editor.IStandaloneCodeEditor) => {
-    editor.focus()
-    bindCommands(editor)
+  const onDidMount = (_editor: monacoEditor.editor.IStandaloneCodeEditor) => {
+    setEditor(_editor)
+    _editor.focus();
+    bindCommands(_editor);
   };
 
   return (
-    <SplitPane split="vertical">
-      <Pane>
-        <div id="monaco-wrapper" style={{ height: "100vh", opacity: 0.9 }}>
-          <MonacoEditor
-            language="javascript"
-            theme="vs-dark"
-            value={code}
-            options={{
-              selectOnLineNumbers: true,
-              automaticLayout: true,
-            }}
-            editorDidMount={onDidMount}
-          />
-        </div>
-      </Pane>
-      <Pane style={{ background: "white" }}>
-        <SheetMusicViewer options={{ autoResize: true }} file={sampleFile} />
-      </Pane>
-    </SplitPane>
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <SplitPane split="vertical" onChange={() => {
+        if (editor) {
+          editor.layout()
+        }
+      }}>
+        <Pane>
+          <div id="monaco-wrapper" style={{ height: "100vh" }}>
+            <MonacoEditor
+              language="javascript"
+              theme="vs-dark"
+              value={code}
+              options={{
+                selectOnLineNumbers: true,
+                automaticLayout: true,
+              }}
+              editorDidMount={onDidMount}
+            />
+          </div>
+        </Pane>
+        <SplitPane split="horizontal">
+          <Pane>
+            <div style={{ height: "100%" }}>
+              <SheetMusicViewer options={{ autoResize: true }} file={sampleFile} />
+            </div>
+          </Pane>
+          <Pane>
+            <div style={{ height: '100%' }}>
+              drop area
+            </div>
+          </Pane>
+        </SplitPane>
+      </SplitPane>
+    </div>
   );
 };
 
