2a1e6362e0a84d725733e5f18eb488560ba16b9b
Yuichi Yogo <yuichkun@gmail.com>
2021-04-17 09:36:36 +0900

    build(utils): exclude test files from bundles

--- yogo/packages/utils/build.js
+++ yogo/packages/utils/build.js
@@ -4,12 +4,14 @@ const watch = process.env.WATCH === 'true'
 console.log(`Building @dawlet/utils with esbuild.\nWatch Option: ${watch}`)
 
 const glob = require('glob')
-const entryPoints = glob.sync('./src/**/*.ts')
+const entryPoints = glob.sync('./src/**/!(*.test).ts')
 build({
   entryPoints,
   platform: 'node',
+  bundle: true,
   outbase: './src',
   outdir: './lib',
   watch,
-  logLevel: 'error'
+  logLevel: 'error',
+  target: 'esnext'
 })
